cde00c6cd840188042ec61dfe9a26fdfa9384089
Grimels <grimels.ua@gmail.com>
2018-12-15 23:46:41 +0200

    lodash/throttle localStorage update not less than once a second

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ import Search from "./containers/Search";
 import { Provider } from "react-redux";
 import storeCreator from "./store";
 import { loadState, saveState } from "./localStorage";
+import throttle from "lodash/throttle";
 let store = storeCreator(loadState());
 
-store.subscribe(() => {
-  saveState(store.getState());
-});
+store.subscribe(
+  throttle(() => {
+    saveState(store.getState());
+  }),
+  1000
+);
 
 class App extends Component {
   render() {
